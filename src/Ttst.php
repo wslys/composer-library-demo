e4fc9754cdcd3a16c1e04565e80dc9df04e4d340
wslgit <company@gmail.com>
2020-01-18 16:13:06 +0800

    one

---
+++ src/Ttst.php
@@ -0,0 +1,15 @@
+<?php
+namespace wslys\comdemo;
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2020/1/18
+ * Time: 16:10
+ */
+
+class Ttst
+{
+    public function EchoTtst($str='') {
+        echo $str;
+    }
+}
